174a7b2528d8e01a421c18988b6939d680724135
Kyle Chen <kyle.h.chen@gmail.com>
2017-10-26 17:13:04 -0700

    Complete 99cats auth in-class assignment (no bonus)

---
+++ ninety_nine_cats_auth/db/migrate/20171027000659_create_sessions.rb
@@ -0,0 +1,12 @@
+class CreateSessions < ActiveRecord::Migration[5.1]
+  def change
+    create_table :sessions do |t|
+      t.string :session_token, null: false
+      t.integer :user_id, null: false
+
+      t.timestamps
+    end
+    add_index :sessions, :session_token, unique: true
+    add_index :sessions, :user_id
+  end
+end
